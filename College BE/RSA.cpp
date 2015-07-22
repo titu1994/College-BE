289bddddc1b2865e5b75a2c8b49d2ab408d7b897
Somshubra Majumdar <titu1994@gmail.com>
2015-07-22 22:32:06 +0530

    Added RSA code in C++

---
+++ College BE/RSA.cpp
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+using namespace std;
+
+long int p, q, n, phi_n, flag, e[100], d[100], temp[100], j, m[100], en[100], i;
+char msg[100];
+int prime(long int);
+void calculate_e();
+long int calculate_d(long int);
+void encrypt();
+void decrypt();
+
+int prime(long int pr)
+{
+	int i;
+	j = sqrt(pr);
+	for (i = 2; i <= j; i++)
+	{
+		if (pr % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	cout << "\nENTER FIRST PRIME NUMBER\n";
+	cin >> p;
+	flag = prime(p);
+	if (flag == 0)
+	{
+		cout << "\nWRONG INPUT\n";
+		exit(1);
+	}
+	cout << "\nENTER ANOTHER PRIME NUMBER\n";
+	cin >> q;
+	flag = prime(q);
+	if (flag == 0 || p == q)
+	{
+		cout << "\nWRONG INPUT\n";
+		exit(1);
+	}
+	cout << "\nENTER MESSAGE\n";
+	fflush(stdin);
+	cin >> msg;
+	for (i = 0; msg[i] != NULL; i++)
+		m[i] = msg[i];
+	n = p * q;
+	phi_n = (p - 1) * (q - 1);
+	calculate_e();
+	cout << "\nPOSSIBLE VALUES OF e AND d ARE\n";
+	for (i = 0; i < j - 1; i++)
+		cout << e[i] << "\tphi_n " << d[i] << "\n";
+	encrypt();
+	decrypt();
+	system("pause");
+	return 0;
+}
+void calculate_e()
+{
+	int k;
+	k = 0;
+	for (i = 2; i < phi_n; i++)
+	{
+		if (phi_n % i == 0)
+			continue;
+		flag = prime(i);
+		if (flag == 1 && i != p && i != q)
+		{
+			e[k] = i;
+			flag = calculate_d(e[k]);
+			if (flag > 0)
+			{
+				d[k] = flag;
+				k++;
+			}
+			if (k == 99)
+				break;
+		}
+	}
+}
+long int calculate_d(long int x)
+{
+	long int k = 1;
+	while (1)
+	{
+		k = k + phi_n;
+		if (k % x == 0)
+			return (k / x);
+	}
+}
+void encrypt()
+{
+	long int pt, ct, key = e[0], k, len;
+	i = 0;
+	len = strlen(msg);
+	while (i != len)
+	{
+		pt = m[i];
+		pt = pt - 96;
+		k = 1;
+		for (j = 0; j < key; j++)
+		{
+			k = k * pt;
+			k = k % n;
+		}
+		temp[i] = k;
+		ct = k + 96;
+		en[i] = ct;
+		i++;
+	}
+	en[i] = -1;
+	cout << "\nTHE ENCRYPTED MESSAGE IS\n";
+	for (i = 0; en[i] != -1; i++)
+		printf("%c", en[i]);
+}
+void decrypt()
+{
+	long int pt, ct, key = d[0], k;
+	i = 0;
+	while (en[i] != -1)
+	{
+		ct = temp[i];
+		k = 1;
+		for (j = 0; j < key; j++)
+		{
+			k = k * ct;
+			k = k % n;
+		}
+		pt = k + 96;
+		m[i] = pt;
+		i++;
+	}
+	m[i] = -1;
+	cout << "\nTHE DECRYPTED MESSAGE IS\n";
+	for (i = 0; m[i] != -1; i++)
+		printf("%c", m[i]);
+}
